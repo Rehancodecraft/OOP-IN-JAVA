50eabe0228eee5cb6f785d26fe3fc44435741d63
rehancodecraft <rehan.codecraft@gmail.com>
2025-02-07 11:55:53 +0500

    Added new classes of OOp2

---
+++ OOP2/com/InnerClasses.java
@@ -0,0 +1,16 @@
+package com;
+public class InnerClasses {
+	static class Test {
+		 String name;
+		public Test(String name) {
+			this.name = name;
+		}
+	}
+	public static void main(String[] args){
+		Test a = new Test("Rehan");
+		Test b = new Test ("Noman");
+
+		System.out.println(a.name);
+		System.out.println(b.name);
+	}
+}
