09b5d8aad3f6c45d7dabeb7a37945e39e7ec2ff5
rehancodecraft <rehan.codecraft@gmail.com>
2025-02-14 01:07:00 +0500

    Added First Lecture of OOP

---
+++ Lecture1/constructor/Main.java
@@ -0,0 +1,10 @@
+package constructor;
+public class Main{
+	public static void main(String[] args){
+		Person P1 = new Person(11, "Rehan Shafiq",19);
+		Person P2 = new Person(12, "Uzair Usman",20);
+		
+		P1.display();
+		P2.display();
+	}
+}
---
+++ Lecture1/constructor/Person.java
@@ -0,0 +1,22 @@
+
+package constructor;
+public class Person{
+	//We have make these datamembers public to accessible from everywhere
+	public int id;
+	public String name;
+	public int age;
+	//This is constructor
+	Person(int id, String name, int age){
+		this.id = id;
+		this.name = name;
+		this.age = age;
+
+	}
+	public void display(){
+		System.out.println("ID: "+ id);
+
+		System.out.println("Name: "+ name);
+		System.out.println("Age: "+ age);
+	}
+}
+
